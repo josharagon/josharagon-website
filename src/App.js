e8be4c3496a2c589d1a99ed28b22a01ca11a6a71
Josh Aragon <josharagon99@gmail.com>
2021-03-24 17:14:39 -0600

    Add light and dark theme button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
 import './App.scss';
+import lightTheme from './img/lightTheme.svg'
+import darkTheme from './img/darkTheme.svg'
 
 function App() {
   return (
 <>
 <nav className='nav-bar'>
-  <h1>Josh Aragon</h1>
-  <div classNam='nav-links'>
-    <h1>About me</h1>
-    <h1>My Work</h1>
-    <h1>Contact Me</h1>
-    <button>Dark Theme</button>
+  <a>Josh Aragon</a>
+  <div className='nav-links'>
+    <a href='#aboutMe'>About me</a>
+    <a href='#myWork'>My Work</a>
+    <a href='#contactForm'>Contact Me</a>
+    <button type='button'><img src={darkTheme} className='theme-logo'></img></button>
   </div>
 </nav>
 </>
