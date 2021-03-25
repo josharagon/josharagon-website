bce7f7479ec03ea2f24a8596bc31158263105e8a
Josh Aragon <josharagon99@gmail.com>
2021-03-25 01:08:12 -0600

    Add dark mode button with working toggle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import './App.scss';
-import lightTheme from './img/lightTheme.svg'
-import darkTheme from './img/darkTheme.svg'
+import * as React from 'react';
+import { useState } from 'react';
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./globalStyles.js";
+import { lightTheme, darkTheme } from "./Themes.js"
 
-function App() {
+const App = (props) => {
+  console.log(props)
+  const [isDarkMode, setDarkMode] = useState(false);
+  const [theme, setTheme] = useState('light');
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+    toggleDarkMode(theme === 'light' ? (true) : (false))
+}
+  const toggleDarkMode = (checked: boolean) => {
+    setDarkMode(checked);
+  };
+
+
   return (
-<>
-<nav className='nav-bar'>
-  <a>Josh Aragon</a>
-  <div className='nav-links'>
-    <a href='#aboutMe'>About me</a>
-    <a href='#myWork'>My Work</a>
-    <a href='#contactForm'>Contact Me</a>
-    <button type='button'><img src={darkTheme} className='theme-logo'></img></button>
-  </div>
-</nav>
-</>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <nav className='nav-bar'>
+          <a href=''>Josh Aragon</a>
+          <div className='nav-links'>
+            <a href='#aboutMe'>About me</a>
+            <a href='#myWork'>My Work</a>
+            <a href='#contactForm'>Contact Me</a>
+            <DarkModeSwitch
+              style={{ marginBottom: '.35rem' }}
+              checked={isDarkMode}
+              size={30}
+              onClick={themeToggler}
+            />
+          </div>
+        </nav>
+        <section className='hello-container'>
+
+        </section>
+      </>
+    </ThemeProvider>
   );
+
 }
 
 export default App;
